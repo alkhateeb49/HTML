18b2b41dea31e5560a920c063c8638e5511da0ca
alkhateeb49 <mohamadalkhateeb49@yahoo.com>
2020-12-16 16:30:16 +0200

    lab8

---
+++ lab8/JavaScript.js
@@ -0,0 +1,15 @@
+
+var ask="";
+while(ask!=="10"){
+ask = prompt("5 + 5 =?");
+}
+
+var q2 = prompt("How maany users you need?");
+users(q2);
+
+
+function users(usernumber){
+for(var i=0;i<usernumber;i++){
+  document.write('<img src="https://assets.stickpng.com/images/585e4bf3cb11b227491c339a.png" style="width:10%;">');
+}
+}
